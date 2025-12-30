32c55dcb53a8e2d239c10a2aa93634cf4fe7662a
Ram Chandra <chandraram8178@gmail.com>
2025-12-30 14:44:11 +0530

    Updated pages and assets

--- src/Components/page/Home.jsx
+++ src/Components/page/Home.jsx
@@ -3,32 +3,20 @@ import Mainheader from '../Common/Mainheader'
 import Banner from '../Common/Banner'
 import About from '../Common/about'
 import Features from '../Common/Features'
-<<<<<<< HEAD
 import HowItWorks from '../Common/HowItWorks'
 import Stats from '../Common/Stats'
 import AISupport from '../Common/AISupport'
 import Testimonials from '../Common/Testimonials'
-=======
-
->>>>>>> 67192c1109c039bea1e81e4b41830e968f2c422f
 const Home = () => {
   return (
     <>
      <Mainheader/>
      <Banner/>
-<<<<<<< HEAD
      <Stats/>
      <Features/>
      <HowItWorks/>
      <AISupport/>
      <Testimonials/>
-     
-      
-
-=======
-     <Features/>
-     <About/>
->>>>>>> 67192c1109c039bea1e81e4b41830e968f2c422f
     </>
    
   )
